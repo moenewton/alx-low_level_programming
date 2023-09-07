f49498c4709f140789444cec60b1496c2424e1ea
moenewton <mohamedabdelrahimalbhiry@gmail.com>
2023-09-07 16:10:04 -0700

    a

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,9 +1,10 @@
 #include <stdio.h>
 /**
  * main - My program
- */
+ * Return: 0
+*/
 int main(void)
 {
-	puts("\"Programming is like building a multilingual puzzle");
-		return(0);
+puts("\"Programming is like building a multilingual puzzle");
+return (0);
 }
