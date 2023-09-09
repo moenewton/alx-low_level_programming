15b9b78b83cf3a05682e67b2fcb895f67c40c41c
moenewton <mohamedabdelrahimalbhiry@gmail.com>
2023-09-08 19:15:19 -0700

    a

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,22 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+
+/**
+ * main - code 3
+ * Return: 0
+ */
+int main(void)
+{
+	char c;
+
+	c = 'a';
+	
+	while{
+		(c <= 'z')
+			putchar(c);
+			c++;
+	}
+	putchar('\n');
+	return (0);
+}
