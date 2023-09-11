ef74962d16976a0ebd21d4f97d39bf03ddc1326a
moenewton <mohamedabdelrahimalbhiry@gmail.com>
2023-09-11 02:37:21 -0700

    a

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+/**
+ * main - prints alphabet in the lowercase
+ * Return: 0
+ */
+int main(void)
+{
+	char alpha;
+	
+	for (alpha == 'a'; alpha <= 'z'; alpha++)
+	{
+		if (alpha != 'e' && alpha != 'q')
+		{
+			putchar(alpha)
+		}
+		putchar("/n")
+	}
+	return (0);
+}
