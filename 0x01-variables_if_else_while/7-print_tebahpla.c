b54538a7d47a8b7e8c40d45a45ba4392e98f6052
moenewton <mohamedabdelrahimalbhiry@gmail.com>
2023-09-11 03:30:37 -0700

    a

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/**
+ * main - Prints the alphabet in lowercase, except for q and e.
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+	char alpha;
+
+	for (alpha = 'a'; alpha <= 'z'; alpha--)
+	{
+			putchar(letter);
+	}
+
+	putchar('\n');
+
+	return (0);
+}
