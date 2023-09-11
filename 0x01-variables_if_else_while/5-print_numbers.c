e5512956430716d52ab3dd2ebe02d3dc04e5234b
moenewton <mohamedabdelrahimalbhiry@gmail.com>
2023-09-11 03:14:24 -0700

    a

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+ * main - numbers list
+ * Return: 0
+ */
+int main(void)
+{
+	int number;
+
+	for (number = 0; number < 10; number++)
+	{
+		printf("%d", number);
+	}
+	printf("/n");
+	return (0);
+}
