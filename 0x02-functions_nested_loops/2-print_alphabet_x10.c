bf615772089697b3c1b29511a985543709bb7f9a
moenewton <mohamedabdelrahimalbhiry@gmail.com>
2023-09-12 16:43:16 -0700

    c

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -1,9 +1,9 @@
 # include "main.h"
 /**
-* print_alphabet_10x - p alpahbet
+* print_alphabet_x10 - p alpahbet
 * Return: void
 */
-void print_alphabet_10x(void)
+void print_alphabet_x10(void)
 {
 	char alpha;
 	
