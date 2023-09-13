b950520388acf7a4a11efe59a6f00e7be8668b74
moenewton <mohamedabdelrahimalbhiry@gmail.com>
2023-09-13 08:35:17 -0700

    a

---
+++ 0x02-functions_nested_loops/101-natural.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+*main - print multiples
+*Return: 0
+*/
+int main(void)
+{
+int x;
+sum = 0;
+for (x = 0; x < 1024; x++)
+{
+if ((x % 3) == 0 || (x % 5) == 0)
+sum += x;
+}
+printf("%d\x", sum);
+return (0);
+}
