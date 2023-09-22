9f8b61529cb38837a879e9b12d87afad4343ccdc
moenewton <mohamedabdelrahimalbhiry@gmail.com>
2023-09-21 22:00:42 -0700

    a

---
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+ * _strcat - concatenates 
+ * @dest: string 1
+ * @src: string 2
+ *
+ * Return: a pointer 
+ */
+char *_strcat(char *dest, char *src)
+{
+	int i, j;
+
+	i = 0;
+	j = 0;
+
+	while (dest[i] != '\0')
+		i++;
+
+	while (src[j] != '\0')
+	{
+		dest[i] = src[j];
+		j++;
+		i++;
+	}
+
