f30d449913a3bb01119b05a0623dbe5ed94a2347
moenewton <mohamedabdelrahimalbhiry@gmail.com>
2023-09-28 18:41:49 -0700

    a

---
+++ 0x08-recursion/3-factorial.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ *  factorial - outputs factorial
+ *  @n: variable to give factorial
+ *  Return: factorial
+ */
+int factorial(int n)
+{
+        int x;
+        for (i = 2; i <= x; i++) {
+        n *= i;
+    }
+ 
+    return n;
+}
