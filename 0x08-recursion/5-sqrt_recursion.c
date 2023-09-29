7000ada9c3ea7ae6dcc4fb5969ba2d1a14e74ecc
moenewton <mohamedabdelrahimalbhiry@gmail.com>
2023-09-29 15:18:32 -0700

    a

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -14,4 +14,15 @@ int _sqrt_recursion(int n)
 	}
 	return (actual_sqrt_recursion(n, 0));
 }
-
+int actual_sqrt_recursion(int n, int i)
+{
+	if (i * i == n)
+	{
+		return (i);
+	}
+	else if (i * i < n)
+	{
+		return (-1);
+	}
+	return (actual_sqrt_recursion(n, i + 1));
+}
