40a15346d3ee2c51e6860ddcc4f7293ddff68ec2
moenewton <mohamedabdelrahimalbhiry@gmail.com>
2023-10-04 14:55:09 -0700

    a

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -9,4 +9,13 @@
 char *create_array(unsigned int size, char c)
 {
 	char *str;
+        unsigned int i;
+
+	str = malloc(sizeof(char) * size);
+	if (size == 0 || str == NULL)
+		return (NULL);
+	i = 0;
+	for (; i < size; i++)
+		str[i] = c;
+	return (str);
 }
