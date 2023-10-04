1d704b27a808cc733492c65592ae49dbb021c58e
moenewton <mohamedabdelrahimalbhiry@gmail.com>
2023-10-04 14:41:40 -0700

    a

---
+++ 0x0B-malloc_free/0-create_array.c
@@ -0,0 +1,12 @@
+#include <stdlib.h>
+#include "main.h"
+/**
+ * create_array - create an array
+ * @size: arrray size
+ * @c: char 
+ * Return: NULL
+ */
+char *create_array(unsigned int size, char c)
+{
+	char *str;
+}
