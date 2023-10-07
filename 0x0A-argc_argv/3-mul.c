a6dfd69bdd7f35ac80cf615c7e45c6d2ddda04e8
moenewton <mohamedabdelrahimalbhiry@gmail.com>
2023-10-07 14:48:09 -0700

    a

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -5,17 +5,18 @@
  * @s: string to be converted
  * Return: the int converted from the string
  */
+
 int main(int argc, char *argv[])
 {
-	int x, y;
+	int a, b;
 
 	if (argc != 3)
 	{
 		printf("Error\n");
 		return (1);
 	}
-	x = _atoi(argv[1]);
-	y = _atoi(argv[2]);
-	printf("%d\n", x * y);
+	a = _atoi(argv[1]);
+	b = _atoi(argv[2]);
+	printf("%d\n", a * b);
 	return (0);
 }
